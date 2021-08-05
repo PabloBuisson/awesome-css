f2b3460329e08cf24a1d50473db00aeec8fb7e42
Pablo Buisson <pablo.buisson@gmail.com>
2021-08-05 18:04:02 +0200

    add content about Variables section

--- main.js
+++ main.js
@@ -62,11 +62,11 @@ function hightlightButtonTheme(hue) {
 
 // Change light theme
 const buttonTheme = document.querySelector("#btn-theme");
+const buttonThemeExample = document.querySelector("#btn-example-theme");
 const activateDarkModeMessage = "Activate dark mode";
 const activateLightModeMessage = "Activate light mode";
 
 buttonTheme.addEventListener("click", (e) => {
-    console.log("Switching theme");
     // if dark mode is enabled
     if (buttonTheme.getAttribute("aria-pressed") === "true") {
         setTheme(false, "light", activateDarkModeMessage);
@@ -75,14 +75,28 @@ buttonTheme.addEventListener("click", (e) => {
     }
 });
 
+buttonThemeExample.addEventListener("click", (e) => {
+    // if dark mode is enabled
+    if (buttonThemeExample.textContent === activateLightModeMessage) {
+        setTheme(false, "light", activateDarkModeMessage);
+    } else {
+        setTheme(true, "dark", activateLightModeMessage);
+    }
+})
+
 function setTheme(isButtonPressed, themeName, themeMessage) {
     buttonTheme.setAttribute("aria-pressed", isButtonPressed.toString());
     buttonTheme.setAttribute("title", themeMessage);
     buttonTheme.setAttribute("aria-label", themeMessage);
     document.documentElement.setAttribute("theme", themeName);
+    // change light theme example
+    buttonThemeExample.textContent = themeMessage;
     // save preferences in local storage
     localStorage.setItem("light-theme", themeName);
 }
+
+// change light theme example
+// const buttonTheme = document.querySelector("#btn-example-theme");
 
 /* NAVIGATION LINKS */
 
